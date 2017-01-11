3421537b4712c13d2e8721664d56b5d84a82ab42
kjige <chrisleongsc2@gmail.com>
2017-01-10 21:03:21 -0600

    part1 update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,16 +33,19 @@ function checkItem(prodID, qty) {
     connection.query('SELECT stock_quantity FROM products WHERE ?', {
         item_ID: prodID
     }, function (err, res) {
-        var stockQty = res[0].stock_quantity;
-        // console.log(res[0].stock_quantity);
         if (err) throw err;
-        if (stockQty > qty) {
-            // console.log('Stock available!' + stockQty + ' ' + qty);
-            updateDB(prodID, qty, stockQty);
-        } else {
-            console.log('Insufficient quantity! ' + stockQty + ' ' + qty);
-        }
+        var stockQty = res[0].stock_quantity;
+        checkStock(prodID, stockQty, qty);
     });
+}
+
+function checkStock(prodID, stockQty, qty) {
+    if (stockQty > qty) {
+        // console.log('Stock available!' + stockQty + ' ' + qty);
+        updateDB(prodID, qty, stockQty);
+    } else {
+        console.log('Insufficient quantity! ' + stockQty + ' ' + qty);
+    }
 }
 
 function updateDB(prodID, qty, stockQty) {
@@ -52,6 +55,7 @@ function updateDB(prodID, qty, stockQty) {
     }, {
         item_ID: prodID
     }], function (err, res) {
+        if (err) throw err;
         console.log('newStockQty ' + newStockQty);
         calcPrice(prodID, qty);
     });
@@ -61,9 +65,9 @@ function calcPrice(prodID, qty) {
     connection.query('SELECT price FROM products WHERE ?', {
         item_ID: prodID
     }, function (err, res) {
+        if (err) throw err;
         var price = res[0].price;
         var total = price * qty;
-        if (err) throw err;
         // console.log(res);
         console.log('Your total is $' + total + '!');
     });
